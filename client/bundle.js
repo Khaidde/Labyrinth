81fdda5a2a504995c0f7235635f384d5d3bd2537
Mayur Sharma <mayurgsharma1@gmail.com>
2020-06-15 21:06:32 -0400

    Displaying opponent character's usernames

--- client/bundle.js
+++ client/bundle.js
@@ -405,14 +405,37 @@ class OpponentPlayer extends Entity {
 		this.name = name;
 		this.socketID = socketID;
 
-		var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		var size = 1;
+		var geometry = new THREE.BoxGeometry( size, size, size );
 		var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+
+		textLoad(this.name);
+
 		this.model = new THREE.Mesh(geometry, material);
 		this.model.position.x = x;
 		this.model.position.y = y;
 		this.model.position.z = z;
 		this.world.scene.add(this.model);
 	}
+	usernameLoad(username){
+		var textLoad = new THREE.FontLoader();
+    var textGeom;
+    textLoad.load( '../Aldo the Apache_Regular.json', function ( font ) {
+        textGeom = new THREE.TextBufferGeometry( username, {
+            font: font,
+            size: length/(4*username.length),
+            height: 0.1,
+            curveSegments: 12,
+            bevelEnabled: false,
+        } );
+        var textMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0xffffff });
+        textMesh = new THREE.Mesh(textGeom, textMat);
+        textMesh.position.x = this.model.position.x;
+        textMesh.position.y = this.model.position.y+BufferMapBlock.LENGTH/5;
+        textMesh.position.z = this.model.position.z;
+        scene.add(textMesh);
+    } );
+
 	updatePlayerPose(x, y, z, rot_x, rot_y) {
 		this.x = x;
 		this.y = y;
