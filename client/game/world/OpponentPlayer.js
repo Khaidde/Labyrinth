ed46a86135822e187ceb9048529efe5435738052
Mayur Sharma <mayurgsharma1@gmail.com>
2020-06-17 13:55:35 -0400

    Another gltf model

--- client/game/world/OpponentPlayer.js
+++ client/game/world/OpponentPlayer.js
@@ -19,13 +19,6 @@ class OpponentPlayer extends Entity {
 		this.world.scene.remove(this.textMesh);
 	}
 	modelLoad(){
-		// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		// var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-		// this.model = new THREE.Mesh(geometry, material);
-		// this.model.position.x = x;
-		// this.model.position.y = y;
-		// this.model.position.z = z;
-		// this.world.scene.add(this.model);
 		var loader = new THREE.GLTFLoader();
 		var self = this;
 		loader.load('client/Models/PREMADE_Helmet/DamagedHelmet.gltf', (gltf) => {
